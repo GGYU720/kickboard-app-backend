7e3fb5a7ffabdbac012cad533aaf3f7bd8ab6001
GGYU720 <mgh930@gmail.com>
2023-05-15 22:19:53 +0900

    create Spring Boot

---
+++ src/main/java/com/kickboardappbackend/KickboardAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kickboardappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickboardAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickboardAppBackendApplication.class, args);
+	}
+
+}
